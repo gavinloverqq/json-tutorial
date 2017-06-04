78caf66640b60c3a04a390df0ddf92209b1a8a37
macbookAir <779483309@qq.com>
2017-06-04 10:10:24 +0800

    note“

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -5,17 +5,17 @@ typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_
 
 typedef struct {
     lept_type type;
-}lept_value;
+}lept_value;//Json值得每个节点
 
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR
-};
+};//解析函数返回值
 
-int lept_parse(lept_value* v, const char* json);
+int lept_parse(lept_value* v, const char* json);//解析Json
 
-lept_type lept_get_type(const lept_value* v);
+lept_type lept_get_type(const lept_value* v);//获取类型？？此函数不太明白
 
 #endif /* LEPTJSON_H__ */
